440a47612548d030560e99bd3dc1d8666578c120
Julio de Lima <iam@juliodelima.com.br>
2020-07-24 07:20:35 -0300

    add: Classes podem ter atributos

---
+++ src/main/java/principal/App.java
@@ -0,0 +1,11 @@
+package principal;
+
+import dto.Viagem;
+
+public class App {
+    public static void main(String[] args) {
+        Viagem primeiraViagem = new Viagem();
+
+        primeiraViagem.destino = "Maringá";
+    }
+}
